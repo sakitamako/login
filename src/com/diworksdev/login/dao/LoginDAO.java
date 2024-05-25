666bb73412dcee6b5136560a745aa71ab8c37560
sakitamako <sa.919.1025.ki@gmail.com>
2024-05-25 17:28:11 +0900

    コメント消した

--- src/com/diworksdev/login/dao/LoginDAO.java
+++ src/com/diworksdev/login/dao/LoginDAO.java
@@ -1,5 +1,4 @@
 package com.diworksdev.login.dao;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,83 +7,34 @@ import java.sql.SQLException;
 import com.diworksdev.login.dto.LoginDTO;
 import com.diworksdev.login.util.DBConnector;
 
-//Actionから送られてきた情報を使ってDBへ問い合わせるファイル
-//問い合わせて取得した値をDTOクラスに格納するファイル
 public class LoginDAO {
-
-	//①クラス、メソッドの定義
-	//LoginDTO型を最後に呼び出し元に渡すので、LoginDTO型を戻り値にしたメソッドを作ります。
-	//Actionクラスの値を引数として受け取ります。
 	public LoginDTO select(String name, String password) throws SQLException {
-
-		//②DBConnectorのインスタンス化
-		//DBへの接続準備、DBと会話するためのコード、これでログインできる
-		//Connectionは特定のデータベースとの接続
 		LoginDTO dto = new LoginDTO();
 		DBConnector db = new DBConnector();
-
-		//③getConnectionの呼び出し（DBと接続する）
 		Connection con = db.getConnection();
 
-		//④sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため）
-		//SELECT データを抽出する
-		//＊ テーブルに含まれる項目全て
-		//FROM 〇〇 〇〇という名前のテーブルからデータを選択する
-		//WHERE ＜条件＞抽出条件を指定
-		//test2_tableに入っているデータuser_name=? password = ?に入る条件を満たしたデータがsqlに代入される
 		String sql = "select * from user where user_name=? and password=?";
 
 		try {
-
-			//⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
-			//定義したSQL文の1番目の?にActionから送られたname、
-			//2番目の?にActionから送られたpasswordがそれぞれ入ります。
 			PreparedStatement ps = con.prepareStatement(sql);
-
-			//⑥sql文の?に入れる値をsetする
 			ps.setString(1, name);
 			ps.setString(2, password);
 
-			//⑦executeQuery()/executeUpdate()で実行
-			//（select文の場合はexectuteQuery()を使う）
-			// select文のSQL文を実行するメソッドで戻り値はResultSet
 			ResultSet rs = ps.executeQuery();
 
-			//ここでは2つのことをしている
-			//下に1行ずらすこと
-			//データが存在していれば戻り値を true で返す・存在しなければ falseで返す
 			if (rs.next()) {
-
-				//⑧結果の処理（select文で取得した値をDTOに格納）
-				//select文でDBから取得した情報をString型に変換してDTOクラスに格納します。
-				//LoginDTOクラスのsetName、setPassword（setter）を利用します。
 				dto.setName(rs.getString("user_name"));
 				dto.setPassword(rs.getString("password"));
 			}
 
-		//処理中にSQL関連のエラーが発生した際に実行する処理
-		//tryの中でエラーが発生した場合、catchが受け取り
 		} catch (SQLException e) {
-
-			//例外がスローされる原因となったエラーまたは動作の説明を返します
 			e.printStackTrace();
 
-		//DB接続を終了する際,必ず書くメソッド
-		//最後に実行されるものを指定するための構文
-		//例外が発生しcatchされてもされなくても、共通してやってほしい処理や、
-		//やらなければいけない処理を書くところ。
 		} finally {
-
-			//⑨con.close()で接続を切る
-			//データベースとの接続をクローズ
-			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
 			con.close();
-
 		}
 
-		//dtoに入った値を呼び出し元であるActionクラスに渡す
 		return dto;
-
 	}
 
 }
